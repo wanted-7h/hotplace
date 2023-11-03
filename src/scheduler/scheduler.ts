f5cdd2fff0777c4a257cedd20a45706c6e120c94
sphy1597 <sphy1597@naver.com>
2023-11-03 21:24:20 +0900

    feat: 업종에 대한 가게 전체 DB에 insert기능 추가

--- src/scheduler/scheduler.ts
+++ src/scheduler/scheduler.ts
@@ -9,32 +9,48 @@ export const dbScheduler = async () => {
   try {
     //업종명
     const SANITTN_BIZCOND_NM = "chifood";
-    const url =
+    const getHeadUrl =
       "https://openapi.gg.go.kr/Genrestrt" +
       SANITTN_BIZCOND_NM +
       "?key=" +
       key +
       "&type=json&pSize=5&pindex=1";
 
-    const response = await axios.get(url);
-    const responseData: RestaurantResponse = response.data;
+    const getHead = await axios.get(getHeadUrl);
+    const headData: RestaurantResponse = getHead.data;
 
-    //[0] = head / [1] = row
-    const restaurants: Restaurant[] = responseData.Genrestrtchifood[1].row;
+    // 데이터 총 개수
+    const total = headData.Genrestrtchifood[0].head[0].list_total_count;
+    // const total = 17; test용 값
+    // 한번에 가져올 데이터 수
+    const chunkSize = 5;
 
-    // DB에 들어갈 데이터들
-    const restaurantInfo = {
-      restaurant_name: restaurants[0].BIZPLC_NM,
-      restaurant_type: restaurants[0].SANITTN_BIZCOND_NM,
-      adress: restaurants[0].REFINE_ROADNM_ADDR,
-      lat: Number(restaurants[0].REFINE_WGS84_LAT),
-      lon: Number(restaurants[0].REFINE_WGS84_LOGT),
-    };
-
-    // insert DB
-    const insertDB = await db.Restaurant.create(restaurantInfo);
-
-    console.log("db에 insert : ", insertDB);
+    for (let idx = 1; idx < total; idx += chunkSize) {
+      console.log(idx, "번째 요청");
+      let url =
+        "https://openapi.gg.go.kr/Genrestrt" +
+        SANITTN_BIZCOND_NM +
+        "?key=" +
+        key +
+        "&type=json&pSize=" +
+        chunkSize +
+        "&pindex=" +
+        Math.floor(idx / chunkSize + 1);
+      console.log("insert : ", url);
+      let response = await axios.get(url);
+      let responseData: RestaurantResponse = response.data;
+      let restaurants: Restaurant[] = responseData.Genrestrtchifood[1].row;
+      for (let i in restaurants) {
+        let restaurantInfo = {
+          restaurant_name: restaurants[i].BIZPLC_NM,
+          restaurant_type: restaurants[i].SANITTN_BIZCOND_NM,
+          adress: restaurants[i].REFINE_LOTNO_ADDR, // 도로명 주소가 없는 식당이 존재 함
+          lat: Number(restaurants[i].REFINE_WGS84_LAT),
+          lon: Number(restaurants[i].REFINE_WGS84_LOGT),
+        };
+        let insertDB = await db.Restaurant.create(restaurantInfo);
+      }
+    }
   } catch (error) {
     console.log("api 호출 중 오류 발생 : ", error);
   }
