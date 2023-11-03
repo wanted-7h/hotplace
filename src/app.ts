023ca194bf96acc5312ddf4687a3bce70ee414b6
심규철 <sphy1597@naver.com>
2023-11-03 16:28:41 +0900

    feat: OpenAPI 호출 및 저장 (#30)
    - fixes #8
    - fixes #9 
    - fixes #10
    
    ---------
    
    Co-authored-by: scarf <greenscarf005@gmail.com>

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
 import dotenv from "dotenv";
 import express from "express";
-import sequelzie from "./db/models";
 import morgan from "morgan";
 import testRouter from "./test/test.router";
+import { dbScheduler } from "./scheduler/scheduler"
+import db from "./db/models";
+import { createExpressEndpoints } from "@ts-rest/express";
+
 dotenv.config();
 
 const app = express();
+const schedule = require('node-schedule')
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,7 +19,7 @@ app.use(morgan("dev"));
 
 app.use("/api", testRouter);
 
-sequelzie
+db.sequelize
   .sync({
     force: true, //임시
   })
@@ -27,4 +32,20 @@ sequelzie
 
 app.listen(3000, () => {
   console.log("Server On");
+  schedule.scheduleJob('0 * * * * *', function(){
+    dbScheduler()
+  });
 });
+
+/*
+*    *    *    *    *    *
+┬    ┬    ┬    ┬    ┬    ┬
+│    │    │    │    │    │
+│    │    │    │    │    └ day of week (0 - 7) (0 or 7 is Sun)
+│    │    │    │    └───── month (1 - 12)
+│    │    │    └────────── day of month (1 - 31)
+│    │    └─────────────── hour (0 - 23)
+│    └──────────────────── minute (0 - 59)
+└───────────────────────── second (0 - 59, OPTIONAL)
+
+*/
--- src/db/models/user.ts
+++ src/db/models/user.ts
@@ -12,51 +12,56 @@ export class User extends Model<
   InferCreationAttributes<User>
 > {
   declare id: CreationOptional<number>;
+  declare password: CreationOptional<string>;
   declare user_id: CreationOptional<string>;
   declare lat: CreationOptional<number>;
   declare lon: CreationOptional<number>;
   declare is_recommend_lunch: CreationOptional<boolean>;
   declare createdAt: CreationOptional<Date>;
   declare updatedAt: CreationOptional<Date>;
-}
 
-export const initUser = (sequelize: Sequelize): void => {
-  User.init(
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
+  static initUser = (sequelize: Sequelize): void => {
+    User.init(
+      {
+        id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+        },
+        user_id: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          primaryKey: true,
+        },
+        password: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        lat: {
+          type: DataTypes.DOUBLE,
+          allowNull: true,
+        },
+        lon: {
+          type: DataTypes.DOUBLE,
+          allowNull: true,
+        },
+        is_recommend_lunch: {
+          type: DataTypes.BOOLEAN,
+          allowNull: false,
+          defaultValue: false,
+        },
+        createdAt: DataTypes.DATE,
+        updatedAt: DataTypes.DATE,
       },
-      user_id: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        primaryKey: true,
-      },
-      lat: {
-        type: DataTypes.DOUBLE,
-        allowNull: true,
-      },
-      lon: {
-        type: DataTypes.DOUBLE,
-        allowNull: true,
-      },
-      is_recommend_lunch: {
-        type: DataTypes.BOOLEAN,
-        allowNull: false,
-        defaultValue: false,
-      },
-      createdAt: DataTypes.DATE,
-      updatedAt: DataTypes.DATE,
-    },
-    {
-      modelName: "User",
-      tableName: "user",
-      timestamps: true,
-      charset: "utf8",
-      collate: "utf8_general_ci",
-      sequelize,
-    }
-  );
-};
+      {
+        modelName: "User",
+        tableName: "user",
+        timestamps: true,
+        charset: "utf8",
+        collate: "utf8_general_ci",
+        sequelize,
+      }
+    );
+  };
+}
