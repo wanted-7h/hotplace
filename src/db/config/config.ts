c54318bb2c172bae7f0d50b864614a7d7bfb8cfa
scarf <greenscarf005@gmail.com>
2023-11-05 11:08:39 +0900

    refactor: 설정 단순화

--- src/db/config/config.ts
+++ src/db/config/config.ts
@@ -2,13 +2,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const config = {
-  // development: {
-  //   username: process.env.DB_USER_NAME || "hotplace_user",
-  //   password: process.env.DB_PASSWORD || null,
-  //   database: process.env.DB_DATABASE || "hotplace",
-  //   host: process.env.DB_HOST || "localhost",
-  //   dialect: "mysql",
-  // },
+  development: {
+    username: process.env.DB_USER_NAME || "hotplace_user",
+    password: process.env.DB_PASSWORD || null,
+    database: process.env.DB_DATABASE || "hotplace",
+    host: process.env.DB_HOST || "localhost",
+    dialect: "mysql",
+  },
   test: {
     username: "hotplace_user",
     password: "1234",
@@ -16,13 +16,13 @@ const config = {
     host: "127.0.0.1",
     dialect: "mysql",
   },
-  // production: {
-  //   username: "root",
-  //   password: null,
-  //   database: "database_production",
-  //   host: "127.0.0.1",
-  //   dialect: "mysql",
-  // },
-};
+  production: {
+    username: "root",
+    password: null,
+    database: "database_production",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+} as const;
 
 export default config;
