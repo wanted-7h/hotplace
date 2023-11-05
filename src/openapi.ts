584d0571796f9763e9b3259df29996590ecdcbce
scarf <greenscarf005@gmail.com>
2023-11-06 07:14:20 +0900

    refactor: 예제 라우터 제거 (#59)

--- src/openapi.ts
+++ src/openapi.ts
@@ -1,7 +1,16 @@
-import { contract } from "./contracts";
+import { initContract } from "@ts-rest/core";
 import { generateOpenApi } from "@ts-rest/open-api";
+import { userContract } from "./user/user_router";
+import { reviewContract } from "./review/review_contract";
 
-export const openApiDocument = generateOpenApi(contract, {
+const c = initContract();
+
+const api = c.router({
+  user: userContract,
+  review: reviewContract,
+});
+
+export const openApiDocument = generateOpenApi(api, {
   info: {
     title: "Posts API",
     version: "1.0.0",
