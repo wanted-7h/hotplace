30e0f200fefb2e4ff0851a08c4a9c48f4d2bc8ef
Yonge2 <99579139+Yonge2@users.noreply.github.com>
2023-11-07 18:35:49 +0900

    refactor: redis connect 보장을 위한 then -> await으로 변경
    Co-authored-by: scarf <greenscarf005@gmail.com>

--- src/redis.ts
+++ src/redis.ts
@@ -3,9 +3,8 @@ import * as redis from "redis";
 //default local 127.0.0.1:6379
 const redisClient = redis.createClient();
 
-redisClient.connect().then(() => {
-  console.log("redis is connected");
-});
+await redisClient.connect();
+console.log("redis is connected");
 
 redisClient.on("ready", () => {
   console.log("redis is ready");
