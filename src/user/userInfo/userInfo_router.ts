92722f847ed517f40683f893d1b4fd94d83ffb73
Yonge2 <dlwodyd25@naver.com>
2023-11-08 17:28:28 +0900

    refactor: header.user 보장, spread syntax 일부 적용 (#58)

--- src/user/userInfo/userInfo_router.ts
+++ src/user/userInfo/userInfo_router.ts
@@ -10,69 +10,46 @@ export const userInfoRouter = s.router(userInfoContract, {
   //------------------------------------------------------------------------//
   getUserInfo: {
     handler: async ({ headers }) => {
-      const user = headers.user;
-
+      const { userId: _userId, ...userInfo } = headers.user;
       return {
         status: 200,
-        body: {
-          lat: user?.lat,
-          lon: user?.lon,
-          isRecommendLunch: user?.isRecommendLunch,
-        },
+        body: userInfo,
       };
     },
   },
   //------------------------------------------------------------------------//
   updateUserInfo: {
     handler: async ({ headers, body }) => {
-      //headers.user는 미들웨어를 통과하면 보장되는데, 타입 이슈때문에 if header.user를 걸어놓음.
-      //리펙토링 필요
-      if (headers.user) {
-        let user = headers.user;
+      let user = headers.user;
+      let updateCondition: UpdateCondition = {};
 
-        let updateCondition: UpdateCondition = {};
+      //요청 내용만 변경하기 위한 updateCondition
+      if (body.isRecommendLunch != undefined) {
+        updateCondition.is_recommend_lunch = body.isRecommendLunch;
+      }
 
-        if (body.isRecommendLunch)
-          updateCondition.is_recommend_lunch = body.isRecommendLunch;
+      const { isRecommendLunch: _isRecommendLunch, ...latlon } = body;
+      updateCondition = { ...updateCondition, ...latlon };
+      console.log(updateCondition);
 
-        if (body.lat && body.lon) {
-          updateCondition.lat = body.lat;
-          updateCondition.lon = body.lon;
-        }
-
-        if (updateCondition) {
-          const updateResult = await db.User.update(updateCondition, {
-            where: { user_id: user?.userId },
-          });
-          if (updateResult[0]) {
-            const tokenInfo = {
-              userId: user?.userId,
-              lat: body.lat ? body.lat : user?.lat,
-              lon: body.lon ? body.lon : user?.lon,
-              isRecommendLunch: body.isRecommendLunch
-                ? body.isRecommendLunch
-                : user?.isRecommendLunch,
-            };
-            const newAccessToken = createAccessToken(tokenInfo);
-            return {
-              status: 200,
-              body: { accessToken: newAccessToken },
-            };
-          }
-          return {
-            status: 400,
-            body: { error: "변경사항 없음" },
-          };
-        }
+      if (Object.keys(updateCondition).length === 0)
         return {
           status: 400,
           body: { error: "잘못된 요청, 업데이트 정보 없음" },
         };
+
+      try {
+        await db.User.update(updateCondition, {
+          where: { user_id: user.userId },
+        });
+
+        const tokenInfo = { ...user, ...body };
+        const newAccessToken = createAccessToken(tokenInfo);
+
+        return { status: 200, body: { accessToken: newAccessToken } };
+      } catch {
+        return { status: 404, body: { error: "유저정보 변경 실패" } };
       }
-      return {
-        status: 401,
-        body: { error: "잘못된 접근, 토큰 없음" },
-      };
     },
   },
 });
