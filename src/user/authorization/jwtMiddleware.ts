da3b55bdd9ad08cbd7f8a41c7f7e183e34d3e1ab
Yonge2 <dlwodyd25@naver.com>
2023-11-08 11:48:59 +0900

    style: 포메팅 적용

--- src/user/authorization/jwtMiddleware.ts
+++ src/user/authorization/jwtMiddleware.ts
@@ -6,12 +6,12 @@ const jwtAuthorization = (req: any, res: any, next: NextFunction) => {
   const accessToken = req.headers.authorization;
 
   if (accessToken) {
-      const verifiedToken = verifyToken(accessToken);
-      if (verifiedToken.validation) {
-        req.headers.user = verifiedToken.payload;
-        return next();
-      }
-      return res.status(401).json({ error: "유효하지 않은 토큰" });
+    const verifiedToken = verifyToken(accessToken);
+    if (verifiedToken.validation) {
+      req.headers.user = verifiedToken.payload;
+      return next();
+    }
+    return res.status(401).json({ error: "유효하지 않은 토큰" });
   }
   return res.status(401).json({ error: "토큰 없음" });
 };
